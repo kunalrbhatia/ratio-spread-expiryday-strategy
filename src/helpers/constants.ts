e3d28ee59ba1e75fd9a808c97f1375f2b879113a
Kunal Bhatia <kunal.bhatia@hotmail.com>
2026-06-29 03:44:51 +0530

    fix: update scrip master URL to active endpoint

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,5 +4,5 @@ export const CONSTANTS = {
   NIFTY_SPOT_TOKEN: '99926000', // NSE index token for Nifty 50
   ANGEL_ONE_BASE_URL: 'https://apiconnect.angelone.in',
   SCRIP_MASTER_URL:
-    'https://margincalculator.angelbroking.com/OpenAPI_ScripHeader/OpenAPIScripHeader.json',
+    'https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json',
 };
